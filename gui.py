9c721fc2fe53b6d3197c63af90c72e3004f3b0a6
John Baublitz <boblitz13@gmail.com>
2015-01-09 13:46:00 -0500

    Track add function now working

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import pygtk
 pygtk.require('2.0')
 import gtk
 
-class BSplitFrontEnd:
+class FrontEnd:
     def __init__(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 
@@ -10,12 +10,24 @@ class BSplitFrontEnd:
         self.window.connect('destroy', self.destroy)
 
         self.tracks = []
-        self.add_tracks()
+        self.add_track()
 
         self.window.show()
 
-    def add_track(self):
-        
+    def add_track(self, widget=None, data=None):
+        hbox = gtk.HBox(False, 0)
+        textbox = gtk.Entry(100)
+        button = gtk.Button(stock=gtk.STOCK_DELETE)
+
+        hbox.pack_end(textbox)
+        hbox.pack_end(button)
+
+        self.tracks.append(hbox)
+        self.window.add(hbox)
+
+        hbox.show()
+        textbox.show()
+        button.show()
 
     def delete_event(self, widget, event, data=None):
         return False
@@ -25,3 +37,6 @@ class BSplitFrontEnd:
 
     def main(self):
         gtk.main()
+
+fe = FrontEnd()
+fe.main()
