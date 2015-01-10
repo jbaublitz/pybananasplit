f4cf57635df418fb46be59cb6b52594d54d6dd86
John Baublitz <boblitz13@gmail.com>
2015-01-10 16:12:29 -0500

    Beginning of split implementation

--- gui.py
+++ gui.py
@@ -7,30 +7,41 @@ from wavefuncs import TrackSplitter
 class FrontEnd:
     def __init__(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        self.vbox_main = gtk.VBox(True, 0)
+        self.vbox_main = gtk.VBox(False, 0)
         self.add_button = gtk.Button(stock=gtk.STOCK_ADD)
+        self.button_bar = gtk.HBox(False, 0)
+        self.split_button = gtk.Button(label='Split file')
         self.filename = gtk.HBox(False, 0)
         self.filename_label = gtk.Label('File name: ')
         self.filename_box = gtk.Entry(300)
+        self.tracks = gtk.VBox(False, 0)
 
         self.window.connect('delete_event', self.delete_event)
         self.window.connect('destroy', self.destroy)
+        self.split_button.connect('clicked', self.split_file)
         self.add_button.connect('clicked', self.add_track)
+
+        self.button_bar.pack_start(self.split_button)
 
         self.filename.pack_start(self.filename_label)
         self.filename.pack_start(self.filename_box)
 
+        self.vbox_main.pack_start(self.button_bar)
         self.vbox_main.pack_start(self.filename)
         self.vbox_main.pack_start(self.add_button)
+        self.vbox_main.pack_start(self.tracks)
         self.window.add(self.vbox_main)
 
         self.add_track()
 
         self.vbox_main.show()
+        self.button_bar.show()
+        self.split_button.show()
         self.add_button.show()
         self.filename.show()
         self.filename_label.show()
         self.filename_box.show()
+        self.tracks.show()
 
         self.window.show()
 
@@ -50,7 +61,7 @@ class FrontEnd:
         hbox.pack_start(stop)
         hbox.pack_start(button)
 
-        self.vbox_main.pack_start(hbox)
+        self.tracks.pack_start(hbox)
 
         hbox.show()
         start_label.show()
@@ -63,7 +74,9 @@ class FrontEnd:
         obj.parent.parent.remove(obj.parent)
 
     def split_file(self, obj):
-        fs = FileSplitter()
+        ts = TrackSplitter('Noise.wav')
+        for w in self.tracks:
+            print w
 
     def delete_event(self, widget, event, data=None):
         return False
