0473d0c65852732d8ae0be488a27d60236127913
John Baublitz <boblitz13@gmail.com>
2015-01-15 12:35:26 -0500

    Added input validation for GUI

--- gui.py
+++ gui.py
@@ -2,7 +2,10 @@ import pygtk
 pygtk.require('2.0')
 import gtk
 
-from wavefuncs import TrackSplitter
+import re
+from datetime import datetime, time
+
+from splitter import TrackSplitter
 
 class FrontEnd:
     def __init__(self):
@@ -13,7 +16,7 @@ class FrontEnd:
         self.split_button = gtk.Button(label='Split file')
         self.filename = gtk.HBox(False, 0)
         self.filename_label = gtk.Label('File name: ')
-        self.filename_box = gtk.Entry(300)
+        self.filename_entry = gtk.Entry(300)
         self.tracks = gtk.VBox(False, 0)
 
         self.window.connect('delete_event', self.delete_event)
@@ -24,7 +27,7 @@ class FrontEnd:
         self.button_bar.pack_start(self.split_button)
 
         self.filename.pack_start(self.filename_label)
-        self.filename.pack_start(self.filename_box)
+        self.filename.pack_start(self.filename_entry)
 
         self.vbox_main.pack_start(self.button_bar)
         self.vbox_main.pack_start(self.filename)
@@ -40,7 +43,7 @@ class FrontEnd:
         self.add_button.show()
         self.filename.show()
         self.filename_label.show()
-        self.filename_box.show()
+        self.filename_entry.show()
         self.tracks.show()
 
         self.window.show()
@@ -73,10 +76,68 @@ class FrontEnd:
     def remove_track(self, obj):
         obj.parent.parent.remove(obj.parent)
 
+        # Allow auto-shrink on track removal
+        self.window.resize(1, 1)
+
     def split_file(self, obj):
-        ts = TrackSplitter('Noise.wav')
-        for w in self.tracks:
-            print w
+        filename = self.filename_entry.get_text()
+        try:
+            with TrackSplitter(filename) as ts:
+                rc, ipt = self.validate_input(ts.duration)
+                if not rc:
+                    raise IOError('Invalid input: %s' % ipt)
+                for hbox in self.tracks:
+                    self.create_track(hbox)
+        except IOError, emsg:
+            self.warning_dialog('WAV file split failed: %s' % emsg)
+
+    def validate_input(self, duration):
+        last_track = None
+        for hbox in self.tracks:
+            children = hbox.get_children()
+            start_text = children[1].get_text()
+            stop_text = children[3].get_text()
+
+            invalid_input = None
+            if not re.match(r'^([0-9]+:)?[0-5][0-9]:[0-5][0-9]$', start_text):
+                invalid_input = start_text
+            if not re.match(r'^([0-9]+:)?[0-5][0-9]:[0-5][0-9]$', stop_text):
+                invalid_input = stop_text
+            if invalid_input:
+                return False, 'Incorrect track time format: %s'
+
+            if last_track is not None and int(last_track.replace(':', None)) > \
+                    int(start_text.replace(':', None)):
+                return False, 'Tracks out of order, %s should be before %s' % \
+                        (last_track, stop_text)
+            if last_track is not None and int(start_text.replace(':', None)) > \
+                    int(stop_text.replace(':', None)):
+                return False, 'Tracks out of order, %s should be before %s' % \
+                        (last_track, stop_text)
+
+            last_track = stop_text
+
+        if last_track is not None:
+            fmt = None
+            if len(last_track.split(':')) == 3:
+                fmt = '%H:%M:%S'
+            else:
+                fmt = '%M:%S'
+            time_obj = datetime.strptime(last_track, fmt).time()
+            duration_obj = time(duration // 3600, duration % 3600 // 60, duration % 60)
+
+            if time_obj > duration_obj:
+                return False, 'Ending of last track extends past the end of the WAV file.'
+
+        return True, 'Success'
+
+    def warning_dialog(self, msg):
+        message = gtk.MessageDialog(type=gtk.MESSAGE_WARNING)
+        message.set_markup(msg)
+        message.add_button(gtk.STOCK_OK, gtk.RESPONSE_CLOSE)
+        rc = message.run()
+        if rc == gtk.RESPONSE_CLOSE:
+            message.destroy()
 
     def delete_event(self, widget, event, data=None):
         return False
--- splitter.py
+++ splitter.py
@@ -4,16 +4,20 @@ class TrackSplitter:
     def __init__(self, in_filename):
         self.wave_file = wave.open(in_filename)
         self.params = self.wave_file.getparams()
+        self.framerate = self.params[2]
+        nframes = self.params[3]
+        self.duration = nframes // self.framerate
 
     def __enter__(self):
         return self
 
-    def __exit__(self):
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is not None:
+            print traceback
         self.wave_file.close()
 
     def create_track(self, out_filename, nsecs):
-        framerate = self.params[2]
-        data = self.wave_file.readframes(nsecs * framerate)
+        data = self.wave_file.readframes(nsecs * self.framerate)
 
         new_wave_file = wave.open(out_filename, 'w')
         new_wave_file.setparams(self.params)
