783d16314a1ff126b7faf199c0271db07cef5939
John Baublitz <john.m.baublitz@gmail.com>
2015-04-20 15:18:05 -0400

    Added optional update feature

--- python/bananasplit.py
+++ python/bananasplit.py
@@ -1,20 +1,28 @@
+import os
 import sys
-import gtk
+import git
 
 from gui import FrontEnd
 
 def usage():
     print "Usage:"
-    print "\tbananasplit [FILE]"
+    print "\tbananasplit [UPDATE_DIR]"
     sys.exit(1)
 
 if len(sys.argv) > 2:
     usage()
 
 # Set up arguments
-filename = None
+update_dir = None
 if len(sys.argv) > 1:
-    filename = sys.argv[1]
+    update_dir = sys.argv[1]
 
-fe = FrontEnd(filename)
+if update_dir:
+    g = git.cmd.Git(update_dir)
+    ret = g.pull()
+    for info in ret:
+        if not info.flags & HEAD_UPTODATE:
+            os.execv(__file__, sys.argv)
+
+fe = FrontEnd(None)
 fe.main()
